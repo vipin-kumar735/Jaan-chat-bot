e35c70fcba1041f9450f745b50513b2c3a6ab5e9
LEGENDMICKEY <nursumartosif@gmail.com>
2024-11-18 13:37:07 +0530

    Update Shayri.py

--- nexichat/mplugin/Shayri.py
+++ nexichat/mplugin/Shayri.py
@@ -73,8 +73,8 @@ async def shayri(client: Client, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url="https://t.me/TG_FRIENDSS"),
-                    InlineKeyboardButton("✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url="https://t.me/VIP_CREATORS"),
+                    InlineKeyboardButton("💘𝚂𝚄𝙿𝙿𝙾𝚁𝚃💘", url="https://t.me/VAMPIRE_UPDATEE"),
+                    InlineKeyboardButton("💘ᴠᴀᴍᴘɪʀᴇ💘", url="https://t.me/llMR_VAMPIRE_KINGll"),
                 ]
            ]
         ),
