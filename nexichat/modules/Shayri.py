3ce85179b0166b920c14c5a720294880e291ccb2
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-13 16:39:01 +0530

    Update Shayri.py

--- nexichat/modules/Shayri.py
+++ nexichat/modules/Shayri.py
@@ -73,10 +73,10 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/VAMPIRE_UPDATEE"
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/ksd_bot_network"
                     ),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/VAMPIRE_UPDATEE"
+                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/ll_huntet_lll"
                     ),
                 ]
             ]
