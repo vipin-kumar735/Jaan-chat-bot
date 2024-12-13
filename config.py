f6be1706668c3980f958f3b7d82e3a52f5320d6e
Ksdofficial8 <ksdofficial84@gmail.com>
2024-12-13 15:26:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = 6435225
 # -------------------------------------------------------------
 API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7668520999"))
+OWNER_ID = int(getenv("OWNER_ID", 7668520999))
 SUPPORT_GRP = "TEAM_RIYA_SUPPORT"
 UPDATE_CHNL = "KSD_BOT_NETWORK"
 OWNER_USERNAME = "ll_hunter_lll"
