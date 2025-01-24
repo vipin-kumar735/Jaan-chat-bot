09e3b3c24de365293887724439284717ed08ad81
Rocky9852 <abhisheklkr628@gmail.com>
2025-01-24 17:12:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7833972330:AAErp7yd_GYU9UyvE84tyfvFlb0KctRe9do"
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://theriyamusic94:f67KlgTyzr3TTutn@cluster0.lym5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", 7668520999))
-SUPPORT_GRP = "https://t.me/SaregamaSupport"
-UPDATE_CHNL = "https://t.me/SaregamaUpdate"
-OWNER_USERNAME = "https://t.me/Rocky_dtm"
+SUPPORT_GRP = "SaregamaSupport"
+UPDATE_CHNL = "SaregamaUpdate"
+OWNER_USERNAME = "Rocky_dtm"
