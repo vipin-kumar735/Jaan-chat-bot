a6e4470118e980e03a311ca955624964785e6050
BABY-X-MUSIC <DEEPAKKUMARCSC911@GMAIL.COM>
2025-02-22 19:59:48 +0530

    Update Clone.py

--- nexichat/modules/Clone.py
+++ nexichat/modules/Clone.py
@@ -84,7 +84,7 @@ async def clone_txt(client, message):
         except BaseException as e:
             logging.exception("Error while cloning bot.")
             await mi.edit_text(
-                f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @ll_hunter_lll for assistance**"
+                f"⚠️ <b>Error:</b>\n\n<code>{e}</code>\n\n**Forward this message to @UFC_INOCENT for assistance**"
             )
     else:
         await message.reply_text("**Provide Bot Token after /clone Command from @Botfather.**")
