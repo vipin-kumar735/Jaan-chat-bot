be8a8c54e7248825eafc2e873403aab4b6ce028e
BABY-X-MUSIC <DEEPAKKUMARCSC911@GMAIL.COM>
2025-02-22 19:40:58 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -24,10 +24,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("SaregamaUpdate")
-                await self.one.join_chat("SaregamaSupport")
-                await self.one.join_chat("SaregamaSupport")
-                await self.one.join_chat("SaregamaUpdate")
+                await self.one.join_chat("UFC_NETWORK")
+                await self.one.join_chat("UFC_NETWORK")
+                await self.one.join_chat("UFC_NETWORK")
+                await self.one.join_chat("UFC_NETWORK")
 
             except:
                 pass
