d046fd3f15022da93a0825c616d409c3be93a752
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-03-02 17:35:21 +0530

    ***

--- EsproChat/modules/Shayri.py
+++ EsproChat/modules/Shayri.py
@@ -76,7 +76,7 @@ async def shayri(client: Client, message: Message):
                         "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/EsproUpdate"
                     ),
                     InlineKeyboardButton(
-                        "✨ɪɴɴᴏᴄᴇɴᴛ ʙᴀᴄʜᴀ ✨", url=f"https://t.me/Ur_Haiwan"
+                        "✨𝐇αιωαη [•ᴧғᴋ•] ✨", url=f"https://t.me/Ur_Haiwan"
                     ),
                 ]
             ]
--- EsproChat/mplugin/Shayri.py
+++ EsproChat/mplugin/Shayri.py
@@ -74,7 +74,7 @@ async def shayri(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("💘𝚂𝚄𝙿𝙿𝙾𝚁𝚃💘", url="https://t.me/EsproUpdate"),
-                    InlineKeyboardButton("💘ɪɴɴᴏᴄᴇɴᴛ ʙᴀᴄʜᴀ ⁣⁣⁣⁤💘", url="https://t.me/Ur_Haiwan"),
+                    InlineKeyboardButton("💘𝐇αιωαη [•ᴧғᴋ•] ⁣⁣⁣⁤💘", url="https://t.me/Ur_Haiwan"),
                 ]
            ]
         ),
