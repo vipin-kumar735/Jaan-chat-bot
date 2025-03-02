cf361971060abe93f505961227ac966c9ccbb0b5
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-03-02 17:21:42 +0530

    ***

--- EsproChat/idchatbot/helpers/storage.py
+++ EsproChat/idchatbot/helpers/storage.py
@@ -14,7 +14,7 @@ CHAT_STORAGE = [
     "mongodb+srv://chatbot10:j@cluster0.9esnn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 ]
 
-VIPBOY = MongoCli(random.choice(CHAT_STORAGE))
-chatdb = VIPBOY.Anonymous
+ritik = MongoCli(random.choice(CHAT_STORAGE))
+chatdb = ritik.Anonymous
 chatai = chatdb.Word.WordDb
-storeai = VIPBOY.Anonymous.Word.NewWordDb  
+storeai = ritik.Anonymous.Word.NewWordDb  
