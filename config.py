f40fdd58300cad53ba211ba1976320b853bbdd08
Team Espro <162527089+TeamEspro@users.noreply.github.com>
2025-03-09 12:06:31 +0530

    ***

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = "3bcc137f124bc8518c30d461986e247b"
 BOT_TOKEN = getenv("BOT_TOKEN", "7940019678:AAFnZD4f0dM_PPZm9vbmuv_HGPxw6EjGswo")
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://oliva:oliva123@cluster0.6cohqfv.mongodb.net/?retryWrites=true&w=majority")
-OWNER_ID = int(getenv("OWNER_ID", "7380417336"))
+OWNER_ID = int(getenv("OWNER_ID", "6693611573"))
 SUPPORT_GRP = "EsproSupport"
 UPDATE_CHNL = "EsproUpdate"
 OWNER_USERNAME = "Ur_Haiwan"
