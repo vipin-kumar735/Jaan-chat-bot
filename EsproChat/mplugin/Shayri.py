9e7a9d172abe26915d8e8236e2f721d3ac6a0511
vipin-kumar735 <patelharimohan31@gmail.com>
2025-06-22 12:11:48 +0530

    Update Shayri.py

--- EsproChat/mplugin/Shayri.py
+++ EsproChat/mplugin/Shayri.py
@@ -73,7 +73,7 @@ async def shayri(client: Client, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("💘𝚂𝚄𝙿𝙿𝙾𝚁𝚃💘", url="https://t.me/EsproUpdate"),
+                    InlineKeyboardButton("💘𝚂𝚄𝙿𝙿𝙾𝚁𝚃💘", url="https://t.me/TEAM_CHOR"),
                     InlineKeyboardButton("💘𝐇αιωαη [•ᴧғᴋ•] ⁣⁣⁣⁤💘", url="https://t.me/Ur_Haiwan"),
                 ]
            ]
